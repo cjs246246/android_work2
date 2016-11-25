a5244c0d2a74e2f98f320ff2b07e8f5e6e945e8c
201244027 <show654@naver.com>
2016-11-25 11:09:26 +0900

    mainactivity, xml 추가

--- app/src/main/java/com/example/a403/myapplication/MainActivity.java
+++ app/src/main/java/com/example/a403/myapplication/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.widget.LinearLayoutCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
@@ -15,11 +16,12 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     TextView textView1, textView2;
-    CheckBox checkBox;
+    CheckBox checkBox1;
     LinearLayout line;
     RadioButton radioButton1, radioButton2, radioButton3;
     Button button;
     ImageView imageView;
+    RadioGroup RG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
         textView1 = (TextView)findViewById(R.id.textView1);
         textView2 = (TextView)findViewById(R.id.textView2);
-        checkBox = (CheckBox)findViewById(R.id.checkBox);
+        checkBox1 = (CheckBox)findViewById(R.id.checkBox);
         line = (LinearLayout)findViewById(R.id.line);
         radioButton1 = (RadioButton)findViewById(R.id.radioButton1);
         radioButton2 = (RadioButton)findViewById(R.id.radioButton2);
@@ -37,9 +39,36 @@ public class MainActivity extends AppCompatActivity {
         button = (Button)findViewById(R.id.button);
         imageView = (ImageView)findViewById(R.id.imageView);
 
-        if (checkBox.isChecked()){
-            line.setVisibility(View.VISIBLE);
-        };
-        //dfssdf
+        checkBox1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if(isChecked) {
+                    line.setVisibility(View.VISIBLE);
+                }else{
+                    line.setVisibility(View.INVISIBLE);
+            }
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(radioButton1.isChecked())
+                {
+                    imageView.setImageResource(R.drawable.dog);
+
+                }else if(radioButton2.isChecked())
+                {
+                    imageView.setImageResource(R.drawable.cat);
+
+                }else if(radioButton3.isChecked())
+                {
+                    imageView.setImageResource(R.drawable.rabbit);
+                }
+            };
+        });
+
+
     }
 }
